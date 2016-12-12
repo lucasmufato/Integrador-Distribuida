51e084fd6eb2070fa59847a55aa6c1dc916ee2be
Pablo Cabrera <binchmod777@gmail.com>
2016-12-12 15:16:45 -0300

    Metodo para verificar resultado en servidor (no probado)

--- codigo/servidor/Primario.java
+++ codigo/servidor/Primario.java
@@ -5,6 +5,8 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import mensajes.MensajeLogeo;
 
 public class Primario {
@@ -12,10 +14,16 @@ public class Primario {
 	protected ServerSocket serverSO;
 	private HiloConexionPrimario hiloConexion;
 	private Thread hilo;
+	private MessageDigest sha256;
 	
 	public Primario(){
 		//algo
 		this.CrearReplicador();
+		try {
+			this.sha256 = MessageDigest.getInstance ("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			/* */
+		}
 	}
 	
 	public boolean crearGUI(){
@@ -52,5 +60,31 @@ public class Primario {
 	
 	protected boolean CrearReplicador(){
 		return false;
+	}
+
+	protected boolean verificarResultado(byte[] tarea, byte[] n_sequencia, byte[] limite_superior) throws Exception{
+		/* Comprueba que el hash de la concatenacion de tarea con n_sequencia es menor a limite_superior */
+		/* NO PROBADO */
+		byte[] concatenacion = new byte[tarea.length + n_sequencia.length];	
+		System.arraycopy(tarea, 0, concatenacion, 0, tarea.length);
+		System.arraycopy(n_sequencia, 0, concatenacion, tarea.length, n_sequencia.length);
+		byte[] hash = this.sha256.digest (concatenacion);
+
+		if (hash.length != limite_superior.length) {
+			throw new Exception ("No se puede comparar hash con limite superior, longitudes incompatibles.");
+		}
+
+		boolean es_menor = true;
+		int posicion_comparar = 0;
+
+		while (posicion_comparar < hash.length && es_menor) {
+			if (hash[posicion_comparar] < limite_superior[posicion_comparar]) {
+				posicion_comparar++;
+			} else {
+				es_menor = false;
+			}
+		}
+
+		return es_menor;
 	}
 }
