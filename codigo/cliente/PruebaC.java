c3e410c33cf2ef8ffecb858836235e8278caab90
lucasmufato <l.mufato@gmail.com>
2016-12-20 22:23:18 -0300

    no pude lograr pasarle la tarea a C

--- codigo/cliente/PruebaC.java
+++ codigo/cliente/PruebaC.java
@@ -1,12 +1,9 @@
 package cliente;
 
 import java.io.*;
-import java.util.concurrent.*;
 
 public class PruebaC {
 
-	private BlockingQueue<String> m_queue;
-	private PrintWriter print_out;
 	private BufferedReader bufIn;
 	private PrintWriter printOut;
 	private Process p;
@@ -20,7 +17,9 @@ public class PruebaC {
 	
 	public PruebaC(){
 		crearHiloC();
-		comunicarseC();
+		//comunicarseC();
+		pasarleTarea();
+	    p.destroy() ;
 	}
 	
 	private boolean crearHiloC(){
@@ -29,33 +28,74 @@ public class PruebaC {
 	        Runtime rt = Runtime.getRuntime() ;
 
 	        p = rt.exec("/home/lucas/git/Integrador-Distribuida/codigo/cliente/prueba.exe") ;
+	        //leer del proceso
 	        in = p.getInputStream() ;
+	        bufIn = new BufferedReader(new InputStreamReader(in));
+	        //escribir al proceso
 	        out = p.getOutputStream ();
-	        err = p.getErrorStream();
-
 	        printOut = new PrintWriter(out);
-
-	        m_queue = new ArrayBlockingQueue<String>(10);
-
-	        //send a command to 
-	        printOut.println("hola mundo");
-	        printOut.flush();
-
-	        //p.destroy() ;
+	        //errores
+	        err = p.getErrorStream();
 	    }catch(Exception exc){
 	        System.out.println("Err " + exc.getMessage());
+	        System.out.println("Err " + exc.getLocalizedMessage());
+	        System.out.println("Err " + exc.toString());
+	        
 	    }
 		return true;
 	}
 	
+	private boolean pasarleTarea(){
+		byte[] tareaResolver = {0x0A, 0x03, 0x0F, 0x0B, 0x0A, 0x03, 0x0F, 0x0B, 0x0A, 0x03, 0x0F};	//11bytes
+		byte[] parcial = {0x00};
+		byte[] limite_superior = new byte[32];
+		limite_superior[3] = (byte) 0x80;
+		try {
+			/** 	POR ALGUNA RAZON TENGO Q MANDARLE SI O SI ALGO PRIMERO ANTES DE LEER. SINO SE CUELGA EN LEER, PERO NO LEE NADA	**/
+			this.out.write(tareaResolver);
+			this.out.flush();
+			System.out.println("java: le mande la tarea -> ");
+			for (int i=0; i<11;i++){
+				System.out.print(tareaResolver[i]+" ");
+			}
+			System.out.println();
+			System.out.println("esperando informacion del archivo de debug.");
+			String line = bufIn.readLine();
+			System.out.println(line);
+			//byte[] respuestaC = new byte[tamañoTarea];
+			//in.read(respuestaC);
+			line = bufIn.readLine();
+			System.out.println(line);
+			
+			System.out.println("C: lei la tarea -> ");
+			for (int i=0; i<11;i++){
+				//System.out.println(respuestaC[i]);
+			}
+			
+			//this.out.write(parcial);
+			//this.out.write(limite_superior);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		
+		return true;
+	}
+	
+	
 	private boolean comunicarseC(){
+		//send a command to 
+
+        printOut.println("hola mundo");
+        printOut.flush();
 		String line;
 		boolean bandera= true;
-		bufIn = new BufferedReader(new InputStreamReader(in));
+		System.out.println("esperando..");
 	    while (bandera){
 	    	try{
-	            line = bufIn.readLine();
 
+	            line = bufIn.readLine();
 	            if (line != null){
 	                System.out.println("PruebaC-> " +line);
 	        	}else{
@@ -68,5 +108,4 @@ public class PruebaC {
 	    }
 		return true;
 	}
-	
 }
--- codigo/cliente/prueba.c
+++ codigo/cliente/prueba.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 int main(void) {
-
+/*
 unsigned int byte_read;
 char *string, *tok;
 int cmd_id;
@@ -11,7 +11,7 @@ int cmd_id;
 int len = 64;
 string = (char *) malloc(len + 1);
 
-
+	printf("primera cosa \n");
     byte_read = getline(&string,&byte_read, stdin);
 
     if (byte_read == -1) {
@@ -22,6 +22,38 @@ string = (char *) malloc(len + 1);
     } else {
         printf("echo: %s\n", string);
     }
+ */
  
+	//aca voy a almacenar la tarea que reciba
+	char cadenaTarea[11];
+	//archivo para usar de debuger
+	FILE *archivo = fopen("/home/lucas/git/Integrador-Distribuida/codigo/cliente/debug.txt","w");
+	if(archivo == NULL){
+		printf("no pude abrir el archivo de debug \n");
+	}else{
+		printf("pude abrir el archivo de debug \n ");
+	}
+	
+	//el parcial
+	char *parcial;
+	// tengo q ver como hago para leer el limiteSuperior de java
+	fprintf(archivo,"por leer lo que me mandan. \n");
+	// voy a leer la cadena que es lo primero que me mandan
+	
+	fgets(cadenaTarea, sizeof(cadenaTarea),stdin);
+	
+	fprintf(archivo,"lei: %s \n",cadenaTarea);
+	
+	printf("Soy C: la cadena que lei es: %s \n",cadenaTarea);
+	
+	fprintf(archivo,"le respondi a java \n");
+	
+	//ahora que ya lei la tarea, se que me van a mandar el parcial
+	
+	//y ahora me van a mandar el limite superior
+	
+	//ya lei todo, le indico OK a la clase java y llamo a la funcion que hace la operacion en CUDA
+	fclose(archivo);
+	
 return EXIT_SUCCESS;
 }
