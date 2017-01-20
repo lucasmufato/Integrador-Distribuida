f37bc76bd67b3288a7d853ac7d79491009e68ae6
JasminPaolino <jasminpaolino@gmail.com>
2017-01-20 16:10:00 -0200

    Un poco vista backup (Explota al levantar el segundo servidor)

--- codigo/servidor/Backup.java
+++ codigo/servidor/Backup.java
@@ -1,15 +1,25 @@
 package servidor;
 
+import servidor.vista.ServidorBackupVista;
+
 public class Backup extends Primario {
 	
+	private ServidorBackupVista vistaB;	
 	
 	public Backup(String ip, String puerto) {
 		super(ip, puerto);
+		this.crearGUIBackup();
+		//DESPUES DE QUE YA TENGA LA VISTA, VOY A CREAR EL HILO Q SE COMUNICARA CON REPLICADOR
 		HiloBackup conexionBackup = new HiloBackup();
 		Thread hilo = new Thread(conexionBackup);
 		hilo.start();
 	}
 	
+	private void crearGUIBackup() {
+		//VA A TENER UNA SOLA VISTA, CON LOS BLOQUE, TAREAS E INFO DE ACTUALIZACION QUE RECIBA
+		this.vistaB = new ServidorBackupVista(this);	
+	}
+
 	public boolean pasarAPrimario(){
 		return false;
 	}
